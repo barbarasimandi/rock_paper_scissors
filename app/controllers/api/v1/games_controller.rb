84976b7a419259d97270a4db2879800516d9eddc
Simándi Barbara <barbara.simandi@itron.com>
2022-11-06 13:48:10 +0100

    Add basic game logic with static opponent choice
    	- Handle user_choice from params
    	- Return result with http status code

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,8 +1,37 @@
 module Api
   module V1
     class GamesController < ApplicationController
-      def welcome
-        render json: "Welcome!"
+      def play
+        user_choice = user_choice_params.fetch(:user_choice, "no_choice_provided").to_sym
+        opponent_choice = "rock".to_sym
+
+        result =
+          if rules[opponent_choice].include?(user_choice)
+            "#{user_choice}, vs #{opponent_choice}. Opponent wins!"
+          elsif rules[user_choice].include?(opponent_choice)
+            "#{user_choice}, vs #{opponent_choice}. User wins!"
+          else
+            "#{user_choice}, vs #{opponent_choice}. It's a tie!"
+          end
+
+        render json: result, status: 200
+
+      rescue StandardError => e
+        render json: e.message, status: 422
+      end
+
+      private
+
+      def rules
+        {
+          rock: [:scissors],
+          paper: [:rock],
+          scissors: [:paper]
+        }
+      end
+
+      def user_choice_params
+        params.permit(:user_choice)
       end
     end
   end
--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Games", type: :request do
-  describe "GET /api/v1/welcome" do
-    it "works! (now write some real specs)" do
-      get api_v1_welcome_path
-      expect(response).to have_http_status(200)
+  describe "GET /api/v1/play" do
+    context "with user choice param" do
+      it "has http status 200" do
+        get api_v1_play_path, params: { user_choice: "paper" }
+        expect(response).to have_http_status(200)
+      end
+
+      it "unknown weapon returns http status 422" do
+        get api_v1_play_path, params: { user_choice: "hammer" }
+        expect(response).to have_http_status(422)
+      end
+    end
+
+    context "without user_choice param" do
+      it "returns http status 422" do
+        get api_v1_play_path
+        expect(response).to have_http_status(422)
+      end
     end
   end
 end
